5f64be7d402ee119297f3668bb538668d7318a71
Gathers <cmgathers.2017@mymail.jcsu.edu>
2018-01-31 13:12:43 -0500

    first commit

---
+++ src/Laptop.java
@@ -0,0 +1,17 @@
+
+public class Laptop {
+String laptopBrand;
+double price;
+int year;
+
+	public Laptop(String apple, double price2, int year2) {
+		// TODO Auto-generated constructor stub
+		laptopBrand=apple;
+		price=price2;
+		year=year2;
+		
+	}
+
+	
+
+}
---
+++ src/Laptopt2.java
@@ -0,0 +1,13 @@
+
+public class Laptopt2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Laptop lap2=new Laptop("Apple",1800,2018);
+System.out.println(lap2.laptopBrand);
+Laptop lap3=new Laptop("HP", 1500,2017);
+System.out.println(lap3.laptopBrand);
+lap3.laptopBrand="Acer";
+	}
+
+}
